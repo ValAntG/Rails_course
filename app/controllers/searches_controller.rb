60bd79750f4b9565be080a1a41615c28c0c7b0f7
Valentyn Gevrasov <ValAntG@gmail.com>
2019-08-21 18:08:56 +0300

    Lesson#16 lesson dont complete

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 class SearchesController < ApplicationController
   def show
     search_route
+    @station_first = RailwayStation.find(params[:station_first_id])
+    @station_last = RailwayStation.find(params[:station_last_id])
   end
 
   def new
@@ -14,27 +16,15 @@ class SearchesController < ApplicationController
   end
 
   def search_route
-    @search = {station_first: params[:station_first], station_last: params[:station_last]}
     @search_rsr = []
-    station_first = RailwayStation.find_by(title: params[:station_first])
-    station_last = RailwayStation.find_by(title: params[:station_last])
-    # routes = Route.where(id:  RailwayStationsRoute.where(railway_station_id: params[:station_first_id]).select(:route_id))
-    # .where(id:  RailwayStationsRoute.where(railway_station_id: params[:station_last_id]).select(:route_id))
-    routes = station_first.routes & station_last.routes
+    routes = Route.includes(:railway_stations_routes)
+                  .where(id: RailwayStationsRoute.where(railway_station_id: params[:station_first_id]).select(:route_id))
+                  .where(id: RailwayStationsRoute.where(railway_station_id: params[:station_last_id]).select(:route_id))
     routes.each do |route|
-      station_position, search_first_id = nil, nil
-      route.railway_stations_routes.each do |rsr|
-
-        if rsr.railway_station_id == station_first.id
-          search_first_id = rsr.id
-          station_position = rsr.position
-        end
-
-        if !station_position.nil? && rsr.railway_station_id == station_last.id && station_position < rsr.position
-          search_last_id = rsr.id
-          @search_rsr.push({first_id: search_first_id, last_id: search_last_id})
-        end
-      end
+      rsrs = route.railway_stations_routes
+      firstrsr = rsrs.detect { |rsr| rsr.railway_station_id == params[:station_first_id].to_i }
+      lastrsr = rsrs.detect { |rsr| rsr.railway_station_id == params[:station_last_id].to_i && rsr.position > firstrsr.position }
+      @search_rsr.push({firstrsr: firstrsr, lastrsr: lastrsr, route: route}) if lastrsr
     end
   end
 end
