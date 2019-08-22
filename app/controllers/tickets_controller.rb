0a5ec154328cbc504513ab8175f87dd22febc05b
ValAntG <ValAntG@gmail.com>
2019-08-22 08:39:59 +0300

    Lesson#16 lesson complete

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -16,22 +16,13 @@ class TicketsController < ApplicationController
   end
 
   def create
-    @ticket = Ticket.new(ticket_params)
+    user_id = {user_id: 1}
+    @ticket = Ticket.new((train_params).merge(ticket_params).merge(user_id))
     respond_to do |format|
       if @ticket.save
         format.html { redirect_to @ticket, notice: 'ticket was successfully created.' }
       else
         format.html { render :new }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @ticket.update(ticket_params)
-        format.html { redirect_to @ticket, notice: 'ticket was successfully updated.' }
-      else
-        format.html { render :edit }
       end
     end
   end
@@ -50,5 +41,14 @@ class TicketsController < ApplicationController
 
     def ticket_params
       params.require(:ticket).permit(:last_name, :first_name, :father_name, :passport_series, :passport_numbers)
+    end
+
+    def train_params
+      station_first_id = params[:ticket_rsr_first].to_i
+      station_last_id = params[:ticket_rsr_last].to_i
+      train_id = Train.find(RailwayStationsRoute.find(station_first_id).train_id).id
+      # station_first = RailwayStation.find(RailwayStationsRoute.find(rsr_first).railway_station_id).title
+      # station_last = RailwayStation.find(RailwayStationsRoute.find(rsr_last).railway_station_id).title
+      @train_ticket = { train_id: train_id, railway_station_first_id: station_first_id, railway_station_last_id: station_last_id}
     end
 end
