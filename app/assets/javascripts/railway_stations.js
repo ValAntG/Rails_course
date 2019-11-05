8ed7aee91d18e8c7368619bcec5e31011b02c0fd
Valentyn Gevrasov <ValAntG@gmail.com>
2019-11-05 10:51:18 +0200

    Lesson#20 fix class visibility&invisibility

--- app/assets/javascripts/railway_stations.js
+++ app/assets/javascripts/railway_stations.js
@@ -1,14 +1,33 @@
 $(document).on("turbolinks:load", function () {
-  $('a.edit_button').click(function (e) {
+  $('a.edit_button_first').click(function (e) {
     e.preventDefault();
-    let yourUlAdd = document.getElementsByClassName("edit_form_add");
-    var i;
-    for (i = 0; i < yourUlAdd.length; i++) {
-      yourUlAdd[i].style.display = yourUlAdd[i].style.display === 'none' ? 'block' : 'none';
-    }
-    let yourUlDel = document.getElementsByClassName("edit_form_del")
+
+    let yourUlDel = $(".edit_form_del");
     for (i = 0; i < yourUlDel.length; i++) {
-      yourUlDel[i].style.display = yourUlDel[i].style.display === 'none' ? 'block' : 'none';
-    }
+      $(yourUlDel[i]).addClass('invisibility').removeClass('visibility');
+    };
+
+    let yourUlAdd = $(".edit_form_add");
+    for (i = 0; i < yourUlAdd.length; i++) {
+      $(yourUlAdd[i]).removeClass('invisibility').addClass('visibility');
+    };
   });
 });
+
+$(document).on("turbolinks:load", function () {
+  $('a.edit_button_second').click(function (e) {
+    e.preventDefault();
+
+    let yourUlDel = $(".edit_form_del");
+    for (i = 0; i < yourUlDel.length; i++) {
+      $(yourUlDel[i]).addClass('visibility').removeClass('invisibility');
+    };
+
+    let yourUlAdd = $(".edit_form_add");
+    for (i = 0; i < yourUlAdd.length; i++) {
+      $(yourUlAdd[i]).removeClass('visibility').addClass('invisibility');
+    };
+
+  });
+});
+
